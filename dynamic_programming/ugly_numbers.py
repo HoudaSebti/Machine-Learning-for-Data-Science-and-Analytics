47238519824ca124e356e0c17b21badf6f31a72f
HoudaSebti <sebti.houda14@gmail.com>
2019-06-30 19:49:47 +0200

    minor refactoring

--- dynamic_programming/ugly_numbers.py
+++ dynamic_programming/ugly_numbers.py
@@ -17,7 +17,7 @@ def find_ugly_number(n):
         if ugly_index == 3 * multiplied_by3:
             multiplied_by3 += 1
         if ugly_index == 5 * multiplied_by5:
-            multiplied_by5 += 5
+            multiplied_by5 += 1
 
         multiples = [2 * multiplied_by2, 3 * multiplied_by3, 5 * multiplied_by5]
         index += 1
@@ -27,4 +27,4 @@ def find_ugly_number(n):
 if __name__ == "__main__":
     n = 11
     ugly = find_ugly_number(n)
-    print(" ugly number of rank: ", n, " is: ", ugly)    
+    print(n, "th is: ", ugly)    
