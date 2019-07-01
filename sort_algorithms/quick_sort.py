e84df97faab423717c9a7af257a18bd613da7e46
HoudaSebti <sebti.houda14@gmail.com>
2019-07-01 07:59:04 +0200

    some rearranging

--- sort_algorithms/quick_sort.py
+++ sort_algorithms/quick_sort.py
@@ -1,6 +1,6 @@
 import random
 import sys
-sys.setrecursionlimit(10000)
+sys.setrecursionlimit(40000)
 
 def quick_sort(list, first, last):
     if first < last:
@@ -13,16 +13,13 @@ def quick_sort(list, first, last):
 def adjust_pivot_idx(list, first, last):
 
     pivot = list[last]
-    print("pivot=",pivot)
     i = first - 1
     for j in range(first, last):
         if list[j] <= pivot:
             i=+1
             tmp = list[j]
             list[j] = list[i]
-            print("listj=",list[j])
             list[i] = tmp
-    print("i=", i )
 
     tmp = list[i + 1]
     list[i + 1] = pivot
@@ -33,7 +30,6 @@ def adjust_pivot_idx(list, first, last):
 if __name__ == "__main__":
     list1 = [12, 11, 13, 5]
     list2 = [6, 29, 4]
-    #list3 = [10, 8, 12, 20, 9, 1, 3, 7, 30]
     
     print("list1 before quick sort: ", list1)
     quick_sort(list1, 0, len(list1) - 1)
@@ -43,6 +39,3 @@ if __name__ == "__main__":
     quick_sort(list2, 0, len(list2) - 1)
     print("list2 after quick sort: ", list2)
     
-   # print("list3 before quick sort: ", list3)
-   # quick_sort(list3, 0, len(list3) - 1)
-   # print("list3 after quick sort: ", list3)
