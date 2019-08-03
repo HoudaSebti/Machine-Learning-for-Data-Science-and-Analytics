6844d1cd73ac1211501a27c20ad09ed917a978f2
HoudaSebti <sebti.houda14@gmail.com>
2019-08-03 13:02:43 +0200

    fetching data

--- practical_assignments/linear_regression.py
+++ practical_assignments/linear_regression.py
@@ -2,10 +2,8 @@ import pandas as pd
 from pandas import DataFrame
 import numpy as np
 import matplotlib.pyplot as plt
-import ssl
 
-url="https://archive.ics.uci.edu/ml/machine-learning-databases/undocumented/connectionist-bench/sonar/sonar.all-data"
-df = pd.read_csv(url) #Your code here
+df = pd.read_csv('sonar.all-data') #Your code here
 print("Shape of the dataset: ",df.shape )
 print("Number of unique values in each column: ",df.nunique())
 print("Shape of the dataset: ",df.shape )
