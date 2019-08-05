06dc573016d5335da66c75cf05aea18817495580
HoudaSebti <sebti.houda14@gmail.com>
2019-08-05 08:32:30 +0200

    rearranging data + displaying data description

--- practical_assignments/linear_regression.py
+++ practical_assignments/linear_regression.py
@@ -3,7 +3,12 @@ from pandas import DataFrame
 import numpy as np
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('sonar.all-data') #Your code here
+df = pd.read_csv("data/sonar.all-data") #Your code here
 print("Shape of the dataset: ",df.shape )
 print("Number of unique values in each column: ",df.nunique())
 print("Shape of the dataset: ",df.shape )
+
+pd.options.display.max_columns=100
+pd.options.display.float_format = '{:.4f}'.format
+print("little description: ", df.describe())
+print("the labels are: ", df.iloc[:,-1].unique())
